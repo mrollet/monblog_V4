adbf2b48e5091f6a744df52c9b56e1e7b1ba0eb7
mireille.rollet <mireille.rollet@SIO-SLAM-PROF.BtsSio-Carriat.com>
2016-11-07 09:47:55 +0100

    1er commit

---
+++ Vue/detailsBillet.php
@@ -0,0 +1,20 @@
+<?php ob_start()?>
+   <article>
+    <header>
+        <h1 class="titreBillet"><?= $billet['BIL_TITRE'] ?></h1>
+        <time><?= $billet['BIL_DATE'] ?></time>
+    </header>
+    <p><?= $billet['BIL_CONTENU'] ?></p>
+     </article>
+    <hr />
+     <header>
+        <h1 class="titreReponses">Réponses à <?= $billet['BIL_TITRE'] ?></h1>
+     </header>
+<?php
+foreach ($commentaires as $commentaire):
+    ?>
+    <p><?= $commentaire['COM_AUTEUR'] ?> dit : </p>
+    <p><?= $commentaire['COM_CONTENU'] ?> </p>
+<?php endforeach; ?>
+<?php $contenu = ob_get_clean() ?>
+<?php require 'gabarit.php'; ?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require './Controleur/actions.php';
+
+try {
+    if ((isset($_GET['action'])) && ($_GET['action']=='afficherBillet')) {
+        if (isset($_GET['id'])) {
+            // intval() renvoie la valeur numérique du paramètre ou 0 en cas d'échec
+            $id = intval($_GET['id']);  
+            if ($id != 0) {
+                afficherBillets($id);
+            }
+            else
+                throw new Exception("Identifiant de billet incorrect");
+        }
+        else
+            throw new Exception("Aucun identifiant de billet");
+        }
+      else {
+          listerBillets();
+      }
+}
+catch (Exception $e) {
+    afficherErreur($e->getMessage());
+}
